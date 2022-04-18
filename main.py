a727702501df47db9649b1afded29a403e0db5b6
alwaysvictory724 <alishah.amirali9@gmail.com>
2022-04-19 02:32:04 +1000

    print statements were commented before cleaning

--- main.py
+++ main.py
@@ -12,36 +12,59 @@ data structure
     ]
 }
 """
-#to be decided for whether tuple, list or list of dict.
+
+def get_suburb(state,postcode):
+    #print("state: " + state)
+    #print("postcode " + postcode)
+    items = []
+    [items.append(tup) for tup in postcode_data[state] if postcode in tup[0]]
+    #print(items)
+
+def find_state(postcode):
+    #print("find state postcode: " + postcode)
+    for states in postcode_data:
+        for tup in postcode_data[states]:
+            if postcode in tup[0]:
+                return states
+        
+            
 
 def process_address(addr):
-    no_postcode = False
+    #no_postcode = False
    # print("invoked process address method")
     #print(addr) #process address into comma separated and then write into output file.
     addr1 = addr.split(" ")
     postcode = addr1.pop()
     if postcode.isdigit(): #assuming that postcode is not provided then last pop is the state. 
         state_acronym = addr1.pop()
+        #print("here")
+        if state_acronym not in postcode_data:
+            #print (state_acronym + "not found ")
+            addr1.append(state_acronym) 
+            state_acronym = find_state(postcode)
+            #print(str(state_acronym))
     else:
         state_acronym = postcode
-        no_postcode = True
+        postcode = ""
 
     
 
     suburb = addr1.pop()
     while addr1[len(addr1) - 1].lower() not in street_types:
-        print(addr1)
+        #print(addr1)
         suburb = addr1.pop() + " " + suburb
 
     street = ' '.join(addr1)
-    print(postcode + " | " + state_acronym + " | " + suburb +  " | " + street + " | " )
+    #print(postcode + " | " + state_acronym + " | " + suburb +  " | " + street + " | " )
     
-    with open("files/output.csv", "a+") as file:
-        file.write("{},{},{},{}\n".format(street,suburb,state_acronym,postcode))
-
-
-
-
+    try:
+        get_suburb(state_acronym, str(postcode))
+        with open("files/output.csv", "a+") as file:
+            file.write("{},{},{},{}\n".format(street,suburb,state_acronym,postcode))
+        print("Address: " + addr + " successfully added to file.")
+    except:
+        print("ERROR: Address is not valid or not Australian: " + addr)
+    
 
 def init(): 
     #method to initiate the data such as postcode which is used for verification 
@@ -54,16 +77,16 @@ def init():
             data = line.split(",")
             #print(data[0] + " " + data[1] + " " + data[3])
             if data[3] in postcode_data:
-                postcode_data[data[3]].append( ( (int(data[0])) , data[1] , data[3]) )
+                postcode_data[data[3]].append( ( ((data[0])) , data[1] , data[3]) )
             else:
                 #print("enter here")
-                postcode_data[data[3]] = list(( (int(data[0])) , data[1] , data[3]))
+                postcode_data[data[3]] = list(( ((data[0])) , data[1] , data[3]))
 
     with open("files/streets.csv", "r") as file: 
         [street_types.extend((line).lower().strip().split(",")) for line in file]
         
     
-    print (street_types)
+   # print (street_types)
     return 0
 
 if __name__ == "__main__": 
