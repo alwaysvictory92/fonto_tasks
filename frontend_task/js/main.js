b0de38a3b56f4374c92d81b2c08a1c1f63e6d630
alwaysvictory724 <alishah.amirali9@gmail.com>
2022-04-24 12:52:10 +1000

    completed frontend task

--- frontend_task/js/main.js
+++ frontend_task/js/main.js
@@ -1,4 +1,5 @@
 
+
 const xhr = new XMLHttpRequest();
 xhr.responseType = 'json';
 
@@ -9,6 +10,9 @@ window.onload = function() {
 };
 
 function get_data() {
+    document.getElementById("property_table_body").innerHTML = "";
+    document.getElementById("loading").style.display = "block";
+
     xhr.open('GET', base_url + '/properties/all', true);
     xhr.setRequestHeader("content-type", "json/application");
     xhr.setRequestHeader("x-hasura-user-id", "ASy22JCTOKbqrsF");
@@ -16,19 +20,27 @@ function get_data() {
 
     xhr.onload = function() {
         if (xhr.status != 200) { 
-            alert(`Error ${xhr.status}: ${xhr.statusText}`); 
+            model_set_mode(3);
+            open_property_modal();
         } else { 
+            console.log("returned values");
             properties = xhr.response.properties; //for easier writing. 
+            
+            properties.sort(function (a, b) {
+                return a.id - b.id;
+            });
+            
             var total = 0;
-            var num = 1;
+           // console.log(properties);
             for (obj in properties) {
                 if(!properties[obj].hidden) {
-                    add_property_to_table(num++, properties[obj].address, properties[obj].valuation);
+                    add_property_to_table(properties[obj].id, properties[obj].address, properties[obj].valuation);
                     total += properties[obj].valuation;
                 }
 
             }
             document.getElementById("total_valuation").innerHTML += total.toLocaleString("en-US");
+            document.getElementById("loading").style.display = "none";
         }
     };
 
@@ -45,19 +57,95 @@ function add_property_to_table(id, address, value) {
 
 
 xhr.onerror = function() {
-alert("Request failed");
+    model_set_mode(3);
+    open_property_modal();
 };
 
+function add_property_to_api() { 
 
+    
+    console.log("here i am");
+    var form_data = new FormData(document.querySelector('form'));
+    form_data = {
+        'address' : form_data.get("address"),
+        'valuation' : form_data.get("addr_value")
+    };
+    isValid = true;
+    console.log(form_data);
+    if(form_data.address == "") {
+        document.getElementById("address").setAttribute("class", "error"); 
+        isValid=false;
+    }
+
+    if(form_data.valuation <= 0) {
+        document.getElementById("addr_value").setAttribute("class", "error"); 
+        isValid=false;
+    }
+
+    if(!isValid) {
+        console.log("error form data is not valid");
+        return;
+    }
+
+
+    xhr.open('POST', base_url + '/properties/add', true);
+    xhr.setRequestHeader("content-type", "json/application");
+    xhr.setRequestHeader("x-hasura-user-id", "ASy22JCTOKbqrsF");
+
+    xhr.onload = function() {
+        if (xhr.status != 200) { 
+            model_set_mode(3); 
+        } else { 
+            console.log("i am now here");
+            console.log(xhr.response.add_property["id"]);
+            model_set_mode(2)
+            document.getElementById("property_id").innerHTML = xhr.response.add_property["id"];
+            get_data();
+        }
+    };
+
+    xhr.send(JSON.stringify(form_data));
+}
 
 function open_property_modal() {
-document.getElementById("property_modal").style.display = "block";
+    document.getElementById("property_modal").style.display = "block";
 }
 function close_property_modal() {
-document.getElementById("property_modal").style.display = "none";
-document.getElementById("property_form").reset();
+    document.getElementById("property_modal").style.display = "none";
+    document.getElementById("property_form").reset();
+    document.getElementById("address").setAttribute("class", "");
+    document.getElementById("addr_value").setAttribute("class", "");
+    model_set_mode(1);
 }
 
+function model_set_mode(id) {
+    switch(id) {
+        case 1: 
+            //main modal 
+            document.getElementById("modal_body_form").style.display = "block";
+            document.getElementById("modal_body_success").style.display ="none";
+            document.getElementById("modal_body_error").style.display ="none";
+            change_btn("Add Property", "open_property_modal()");
+            break;
 
+        case 2:
+            document.getElementById("modal_body_success").style.display ="block";
+            document.getElementById("modal_body_error").style.display ="none";
+            document.getElementById("modal_body_form").style.display = "none";
+            change_btn("Close", "close_property_modal()");
+            break;
+        
+        case 3: 
+            document.getElementById("modal_body_error").style.display ="block";
+            document.getElementById("modal_body_success").style.display ="none";
+            document.getElementById("modal_body_form").style.display = "none";
+            change_btn("Close", "close_property_modal()");
+            break;
+    }
+}
 
-
+function change_btn (text, func) {
+    btn = document.getElementById("add_property_btn");
+    btn.setAttribute("onClick", func);
+    btn.innerHTML = text;
+}
